24f993557a1d4029a2f8578f9d2aa482783c77d8
db-231099 <db23myprojects@gmail.com>
2024-05-06 23:33:24 +0530

    customer profile update

--- OnlineFood_main/settings.py
+++ OnlineFood_main/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'menu',
     'marketplace',
     'django.contrib.gis',
+    'customers',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,7 @@ TEMPLATES = [
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
                 'accounts.context_processors.get_google_api',
+                'accounts.context_processors.get_user_profile',
             ],
         },
     },
