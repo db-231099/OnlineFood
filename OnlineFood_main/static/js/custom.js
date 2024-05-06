cab84025f1d563fe23ca89ac1831f882d64b8e93
db-231099 <db23myprojects@gmail.com>
2024-05-06 19:18:47 +0530

    dynamic tax and opening hours

--- OnlineFood_main/static/js/custom.js
+++ OnlineFood_main/static/js/custom.js
@@ -98,7 +98,7 @@ $(document).ready(function(){
                     //subtotal, tax and grand_total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
                 } 
@@ -141,7 +141,7 @@ $(document).ready(function(){
                     //subtotal, tax and grand_total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -175,7 +175,7 @@ $(document).ready(function(){
                     //subtotal, tax and grand_total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -205,11 +205,18 @@ $(document).ready(function(){
     }
 
     //apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    //console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
