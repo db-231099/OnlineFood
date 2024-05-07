d9e91ffad0d07f35621124e51d6b5323ea3a564e
db-231099 <db23myprojects@gmail.com>
2024-05-08 00:03:01 +0530

    customer dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from accounts.utils import detectUser, send_verification_email, send_password_reset_email
+from orders.models import Order
 from vendor.forms import VendorForm
 from vendor.models import Vendor
 from .forms import UserForm
@@ -182,7 +183,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request, 'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+    return render(request, 'accounts/custDashboard.html', context)
 
 
 @login_required(login_url='login')
